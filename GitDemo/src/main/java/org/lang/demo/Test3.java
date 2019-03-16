3340596f677a1da69d7072e674a42bb990125e59
1494003688@qq.com <1494003688@qq.com>
2019-03-16 14:46:16 +0800

    print out update

--- GitDemo/src/main/java/org/lang/demo/Test3.java
+++ GitDemo/src/main/java/org/lang/demo/Test3.java
@@ -3,6 +3,6 @@ package org.lang.demo;
 public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("only for testing.....haha");
+		System.out.println("only for testing.....haha,eeeeee");
 	}
 }
